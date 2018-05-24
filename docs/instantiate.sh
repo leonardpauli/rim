ff4ff59b50219aed4b1990e651e3cb7285538f94
Leonard Pauli <leonardpauli@me.com>
2018-05-24 15:07:17 +0200

    docs: added instantiate.sh; testing: updated structure

---
+++ docs/instantiate.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+base_name="lp-node-module-base"
+subname="module"
+
+base_location="$(script_dir)/.."
+name="$1"
+if [ -z "$name" ]; then echo "no name provided"; sleep 3; exit; fi
+
+# setup repo
+base_location_remote="$(cd "$base_location" && git remote get-url origin)"
+git clone "$base_location" "$name" && cd "$name"
+git remote remove origin
+git remote add "$base_name"'-remote' "$base_location_remote"
+git remote add "$base_name"'-local' "$base_location"
+git checkout -b master
+
+# copy data
+ca_dir="$subname/container/data/ca"
+if [ -d "$base_location/$ca_dir" ]; then
+	mkdir -p "$ca_dir"
+	ln "$base_location/$ca_dir"/* "$ca_dir"/
+fi
+# copying of node_modules might not work well because of links?
+
+# rename
+mv "$base_name".sublime-project "$name".sublime-project
+sed -e 's/"name": "base-'"$subname"'"/"name": "'"$name"'-'"$subname"'"/' -i '' $subname/package.json
+
+# next
+echo "see $base_location/docs/setup.instantiate for next steps"; sleep 3
+open "$base_location"/docs/setup.rim; exit # TODO: open vue-base-setup + base-setup as well?
---
+++ module/__tests__/example.js
@@ -0,0 +1,7 @@
+import lib from 'lp-node-module-base' // see note in /Users/leonardpauli/projects/own/docs/app/node/testing
+
+describe('simple', ()=> {
+	it('adds', ()=> {
+		expect(1+1).toBe(2)
+	})
+})
