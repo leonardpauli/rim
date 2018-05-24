1f264ea4179b41bc28296875de5a85137a2ee8fa
Leonard Pauli <leonardpauli@me.com>
2018-05-24 19:02:07 +0200

    tests: use /src

--- module/__tests__/example.js
+++ module/__tests__/example.js
@@ -1,7 +1,10 @@
-import lib from 'lp-node-module-base' // see note in /Users/leonardpauli/projects/own/docs/app/node/testing
+import lib from 'lp-node-module-base/src' // see note in /Users/leonardpauli/projects/own/docs/app/node/testing
 
 describe('simple', ()=> {
 	it('adds', ()=> {
 		expect(1+1).toBe(2)
+	})
+	it('lib', ()=> {
+		lib('Jajajaja')
 	})
 })
