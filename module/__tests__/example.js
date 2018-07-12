f15770f71fe6125ec8b5faf793f7cec0291efa9f
Leonard Pauli <leonardpauli@me.com>
2018-07-12 22:52:13 +0200

    babelrc: simplify by using @leonardpauli/babel-preset

--- module/__tests__/example.js
+++ module/__tests__/example.js
@@ -1,4 +1,4 @@
-import lib from 'js-module-base/src' // see note in /Users/leonardpauli/projects/own/docs/app/node/testing
+import lib from 'js-module-base/src' // see note in $lpdocs/app/js/testing
 
 describe('simple', ()=> {
 	it('adds', ()=> {
