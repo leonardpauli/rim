ce830822ffb29b070bb6c7ba44747cdc7812da14
Leonard Pauli <leonardpauli@me.com>
2018-07-14 08:48:39 +0200

    expression: copied base from utils/filterExpression

---
+++ module/src/expression/index.js
@@ -0,0 +1,21 @@
+// expression/index.js
+// LeonardPauli/rim
+//
+// Created by Leonard Pauli, mid jul 2018
+// Copyright © Leonard Pauli 2018
+
+import {log} from 'string-from-object'
+import {lexemExtendCopyClean1Level} from '../parser/lexemUtils'
+import {evaluateStr as evaluateStr_} from '../parser/evaluate'
+
+import root from './lexems'
+import './lexemsEvalExt'
+
+export {root}
+export const evaluateStr = (str, ctx = exprCtxDefaultGet())=> evaluateStr_(ctx, str)
+
+export const exprCtxDefaultGet = ()=> ({
+	lexem: lexemExtendCopyClean1Level(root.expr),
+	vars: {},
+	errors: [],
+})
